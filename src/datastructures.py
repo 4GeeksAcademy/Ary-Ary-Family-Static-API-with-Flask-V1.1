a2d27b21cb11555afe941b81c237b90ffaa8bfed
Ary1993 <43453934+Ary1993@users.noreply.github.com>
2024-02-17 11:09:09 +0000

    primer commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,4 +1,3 @@
-
 """
 update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
@@ -8,29 +7,50 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._members = [{'first_name': 'John',
+                          'last_name': self.last_name,
+                          'age': 33,
+                          'lucky_numbers': [7, 13, 22],
+                          'id': self._generate_id()},
+                          {'first_name': 'Jane',
+                          'last_name': self.last_name,
+                          'age': 35,
+                          'lucky_numbers': [10, 14, 3],
+                          'id': self._generate_id()},
+                          {'first_name': 'Jimmy',
+                          'last_name': self.last_name,
+                          'age': 5,
+                          'lucky_numbers': [1],
+                          'id': self._generate_id()}]  # Example list of members
 
-        # example list of members
-        self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+    # Read-only: Use this method to generate random members ID's when adding members into the list
+    def _generate_id(self):
         return randint(0, 99999999)
 
+
     def add_member(self, member):
-        # fill this method and update the return
+        # Fill this method and update the return
         pass
 
     def delete_member(self, id):
-        # fill this method and update the return
+        # Fill this method and update the return
         pass
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        """ 
+        for item in self._members:
+            if id == item["id"]:
+                return {"result": item}
+        return {"message": "No encontrado"} 
+        """
+        results = [item for item in self._members if id == item["id"]]
+        return results
 
-    # this method is done, it returns a list with all the family members
+    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
